c8751150238dbc891287d429bfce0b24dac951e9
fdschepper <333476@student.mboutrecht.nl>
2022-05-25 15:37:28 +0200

    posts on the homepage now display a little content

--- wordpress-5.9.3-nl_NL/wordpress/wp-content/themes/wp-dough-main/template-aboutus.php
+++ wordpress-5.9.3-nl_NL/wordpress/wp-content/themes/wp-dough-main/template-aboutus.php
@@ -13,7 +13,7 @@ $content = get_the_content();
 </head>
 <body>
     <div class="banner">
-        <img src="https://i.guim.co.uk/img/media/6c9304db6968dbcb611c74221d3d9937f8d10d9f/283_23_1478_887/master/1478.jpg?width=1200&height=630&quality=85&auto=format&fit=crop&overlay-align=bottom%2Cleft&overlay-width=100p&overlay-base64=L2ltZy9zdGF0aWMvb3ZlcmxheXMvdGctcmV2aWV3LTIucG5n&s=b98ef7ac4d651c56c5e692aa0c9e4a89" alt="">
+        <img src="https://image.shutterstock.com/image-vector/vector-banner-design-circuit-board-260nw-1233925105.jpg" alt="">
     </div>
 
     <div class="nav">
