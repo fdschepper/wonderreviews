74a5e78a09230308aa8bb2c98dec6f7642634e3c
SimonPras <332959@student.mboutrecht.nl>
2022-05-25 15:38:57 +0200

    changes made to the post page

--- wordpress-5.9.3-nl_NL/wordpress/wp-content/themes/wp-dough-main/template-post.php
+++ wordpress-5.9.3-nl_NL/wordpress/wp-content/themes/wp-dough-main/template-post.php
@@ -13,6 +13,7 @@ $content = get_the_content();
     }
     .post_afbeelding{
         display: flex;
+        flex-direction: row;
     }
     .post_link{
         display: flex;
@@ -22,7 +23,7 @@ $content = get_the_content();
 </style></head>
 <body>
     <div class="banner">
-        <img src="https://i.guim.co.uk/img/media/6c9304db6968dbcb611c74221d3d9937f8d10d9f/283_23_1478_887/master/1478.jpg?width=1200&height=630&quality=85&auto=format&fit=crop&overlay-align=bottom%2Cleft&overlay-width=100p&overlay-base64=L2ltZy9zdGF0aWMvb3ZlcmxheXMvdGctcmV2aWV3LTIucG5n&s=b98ef7ac4d651c56c5e692aa0c9e4a89" alt="">
+    <img src="https://image.shutterstock.com/image-vector/vector-banner-design-circuit-board-260nw-1233925105.jpg" alt="">
     </div>
 
     <div class="nav">
@@ -39,7 +40,9 @@ $content = get_the_content();
 
     <h1 class="post_title"> <?php echo $title; //door echo $title; kan je de titel van de post laten zien ?> </h1>
     <div class="post-container">
-        <p> <?php echo $content; //door echo $content; kan je de dingen die in de post staan laten zien ?></p>
+        <p> <a class="post_link" href="https://kidzcity.nl/?utm_source=www.google.com&amp;utm_medium=organic&amp;utm_content=" target="_blank" rel="noopener">pretpark</a>
+        <p><?php echo $content; ?></p>
+<!--<img class="post_afbeelding" src="https://www.want.nl/wp-content/uploads/2021/03/Among-Us-feat.jpg" alt="among us afbeelding" height="400" />-->
     </div>
 </body>
 
