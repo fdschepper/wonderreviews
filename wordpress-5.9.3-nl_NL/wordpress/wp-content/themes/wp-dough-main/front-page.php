ce61c192b5a40efd5236c5abcfab16b10d66a428
fdschepper <333476@student.mboutrecht.nl>
2022-05-25 14:51:48 +0200

    homepage now displays 9 posts

--- wordpress-5.9.3-nl_NL/wordpress/wp-content/themes/wp-dough-main/front-page.php
+++ wordpress-5.9.3-nl_NL/wordpress/wp-content/themes/wp-dough-main/front-page.php
@@ -1,11 +1,65 @@
 <?php
-/** config for home template **/
+/*
+ * Template Name: Home
+ */
+
+$args = array(
+    'numberposts'=> 9
+);              
 
 $title = get_the_title();
 $content = get_the_content();
+$posts = get_posts($args);
+$links = get_permalink();
 
-$homeButton = ['url' => get_home_url(), 'title' => 'Klik hier voor de homepage', 'class' => null];
-$adminButton = ['url' => get_home_url() . '/wp-admin', 'title' => 'Klik om in de admin dashboard te komen', 'class' => 'admin-btn'];
-$buttonGroup = [$homeButton, $adminButton];
+function DiplayPost($index)
+{
+    
+}
 
-render('views/templates/home.php', ['title' => $title, 'content' => $content, 'buttonGroup' => $buttonGroup]);
+?>
+<head>
+    <style> </style>
+</head>
+<body>
+    <div class="banner">
+        <img src="https://image.shutterstock.com/image-vector/vector-banner-design-circuit-board-260nw-1233925105.jpg" alt="">
+    </div>
+
+    <div class="nav">
+        <li>
+            <a href="">home</a>
+        </li>
+        <li>
+            <a href="">about us</a>
+        </li>
+        <li>
+            <a href="">archive</a>
+        </li>
+    </div>
+    <div class="home-container">
+        <br><br><br>
+        <div class="row"> 
+            <div class="review"> <a href="<?php echo get_permalink($posts[0]->ID); ?>"><?php echo $posts[0]->post_title; ?></a> </div>
+            <div class="review"> <a href="<?php echo get_permalink($posts[2]->ID); ?>"><?php echo $posts[2]->post_title; ?></a> </div>
+            <div class="review"> <a href="<?php echo get_permalink($posts[1]->ID); ?>"><?php echo $posts[1]->post_title; ?></a> </div>
+        </div>
+        <div class="row"> 
+            <div class="review"> <a href="<?php echo get_permalink($posts[3]->ID); ?>"><?php echo $posts[3]->post_title; ?></a> </div>
+            <div class="review"> <a href="<?php echo get_permalink($posts[4]->ID); ?>"><?php echo $posts[4]->post_title; ?></a> </div>
+            <div class="review"> <a href="<?php echo get_permalink($posts[5]->ID); ?>"><?php echo $posts[5]->post_title; ?></a> </div>
+        </div>
+        <div class="row"> 
+            <div class="review"> <a href="<?php echo get_permalink($posts[6]->ID); ?>"><?php echo $posts[6]->post_title; ?></a> </div>
+            <div class="review"> <a href="<?php echo get_permalink($posts[7]->ID); ?>"><?php echo $posts[7]->post_title; ?></a> </div>
+            <div class="review"> <a href="<?php echo get_permalink($posts[8]->ID); ?>"><?php echo $posts[8]->post_title; ?></a> </div>
+        </div>
+    </div>
+
+    <?php $str = "whentsdadsaheimposterissus";
+    if (strlen($str) > 10)
+    $str = substr($str, 0, 7) . '...'; 
+    echo $str?>
+    
+</body>
+
--- wordpress-5.9.3-nl_NL/wordpress/wp-content/themes/wp-dough-main/template-home.php
+++ wordpress-5.9.3-nl_NL/wordpress/wp-content/themes/wp-dough-main/template-home.php
@@ -3,8 +3,19 @@
  * Template Name: Home
  */
 
+$args = array(
+    'numberposts'=> 9
+);              
+
 $title = get_the_title();
 $content = get_the_content();
+$posts = get_posts($args);
+$links = get_permalink();
+
+function DiplayPost($index)
+{
+    
+}
 
 ?>
 <head>
@@ -12,7 +23,7 @@ $content = get_the_content();
 </head>
 <body>
     <div class="banner">
-        <img src="https://i.guim.co.uk/img/media/6c9304db6968dbcb611c74221d3d9937f8d10d9f/283_23_1478_887/master/1478.jpg?width=1200&height=630&quality=85&auto=format&fit=crop&overlay-align=bottom%2Cleft&overlay-width=100p&overlay-base64=L2ltZy9zdGF0aWMvb3ZlcmxheXMvdGctcmV2aWV3LTIucG5n&s=b98ef7ac4d651c56c5e692aa0c9e4a89" alt="">
+        <img src="https://image.shutterstock.com/image-vector/vector-banner-design-circuit-board-260nw-1233925105.jpg" alt="">
     </div>
 
     <div class="nav">
@@ -26,10 +37,29 @@ $content = get_the_content();
             <a href="">archive</a>
         </li>
     </div>
-
-    <h1> <?php echo $title; ?> </h1>
     <div class="home-container">
-        <p> <?php echo $content; ?></p>
+        <br><br><br>
+        <div class="row"> 
+            <div class="review"> <a href="<?php echo get_permalink($posts[0]->ID); ?>"><?php echo $posts[0]->post_title; ?></a> </div>
+            <div class="review"> <a href="<?php echo get_permalink($posts[2]->ID); ?>"><?php echo $posts[2]->post_title; ?></a> </div>
+            <div class="review"> <a href="<?php echo get_permalink($posts[1]->ID); ?>"><?php echo $posts[1]->post_title; ?></a> </div>
+        </div>
+        <div class="row"> 
+            <div class="review"> <a href="<?php echo get_permalink($posts[3]->ID); ?>"><?php echo $posts[3]->post_title; ?></a> </div>
+            <div class="review"> <a href="<?php echo get_permalink($posts[4]->ID); ?>"><?php echo $posts[4]->post_title; ?></a> </div>
+            <div class="review"> <a href="<?php echo get_permalink($posts[5]->ID); ?>"><?php echo $posts[5]->post_title; ?></a> </div>
+        </div>
+        <div class="row"> 
+            <div class="review"> <a href="<?php echo get_permalink($posts[6]->ID); ?>"><?php echo $posts[6]->post_title; ?></a> </div>
+            <div class="review"> <a href="<?php echo get_permalink($posts[7]->ID); ?>"><?php echo $posts[7]->post_title; ?></a> </div>
+            <div class="review"> <a href="<?php echo get_permalink($posts[8]->ID); ?>"><?php echo $posts[8]->post_title; ?></a> </div>
+        </div>
     </div>
+
+    <?php $str = "whentsdadsaheimposterissus";
+    if (strlen($str) > 10)
+    $str = substr($str, 0, 7) . '...'; 
+    echo $str?>
+    
 </body>
 
